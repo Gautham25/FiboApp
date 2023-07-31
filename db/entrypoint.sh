b1217b50844c1991bea757ff3b9d13ea916c6d4e
gautham25 <gautham25@lbl.gov>
2023-07-31 13:41:00 -0700

    Docker compose file and docker db config changes

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -14,4 +14,17 @@ wait_postgresql() {
 }
 wait_postgresql
 
+pg_restore -U postgres -F t -C -d postgres < /tmp/czbio_db_dump.tar
+
+PG_HBA_ENTRY='host  all all 0.0.0.0/0   trust'
+
+# # Path to pg_hba.conf file
+PG_HBA_CONF_PATH="${PGDATA}/pg_hba.conf"
+
+# # Add the entry to pg_hba.conf
+echo "$PG_HBA_ENTRY" >> "$PG_HBA_CONF_PATH"
+echo "listen_addresses = '*'" >> "${PGDATA}/postgresql.conf"
+# Restart PostgreSQL server to apply the changes
+pg_ctl -D ${PGDATA} restart
+
 tail -f /var/log/postgresql/logfile
